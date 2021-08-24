755510f9f872c4b1dc6ebbf6e9a0297dab5bad86
Lucas Corte <corte1994.lc@gmail.com>
2021-08-24 13:33:13 -0300

    tipos basicos python

---
+++ aula_01/tipos_basicos.py
@@ -0,0 +1,14 @@
+# tipos escaláveis
+inteiro: int = 5
+flutuante: float = 6.14
+booleano: bool = True
+
+# tipos compostos
+texto: str = "Olá!"
+
+print(
+    f"O número {inteiro} é um {type(inteiro)}\n"
+    f"O número {flutuante} é um {type(flutuante)}\n"
+    f"O número {booleano} é um {type(booleano)}\n"
+    f"O texto {texto} é um {type(texto)}"
+)
